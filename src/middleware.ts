f95010d27e6c6e602efa9b9bca49215d14d7c282
Thong Dang <thong.dang@coderpush.com>
2025-06-06 16:45:17 +0700

    fix(config): fix cookie domain conflict

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,12 @@
-import { createOryMiddleware } from "@ory/nextjs/middleware"
+import { createOryMiddleware } from "@ory/nextjs/middleware";
 
-import oryConfig from "../ory.config"
+import oryConfig from "../ory.config";
 
 // This function can be marked `async` if using `await` inside
-export const middleware = createOryMiddleware(oryConfig)
+export const middleware = createOryMiddleware({
+  ...oryConfig,
+  forceCookieDomain: process.env.NEXT_PUBLIC_COOKIE_DOMAIN,
+});
 
 // See "Matching Paths" below to learn more
-export const config = {}
+export const config = {};
