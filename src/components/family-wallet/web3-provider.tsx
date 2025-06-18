f19fd9fb60f146929fa41dbfc09beb585d674137
Thong Dang <thong.dang@coderpush.com>
2025-06-18 18:28:54 +0700

    fix: fix web3 config

--- src/components/family-wallet/web3-provider.tsx
+++ src/components/family-wallet/web3-provider.tsx
@@ -1,39 +1,16 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ConnectKitProvider, getDefaultConfig } from "connectkit";
 import { useEffect, useState } from "react";
-import {
-  WagmiProvider,
-  createConfig,
-  http,
-  useAccount,
-  useDisconnect,
-  useSignMessage,
-} from "wagmi";
-import { mainnet } from "wagmi/chains";
+import { WagmiProvider, createConfig, useAccount, useDisconnect, useSignMessage } from "wagmi";
 
 import { getUserWalletRequest } from "../../client/api";
 
 const config = createConfig(
   getDefaultConfig({
-    // Your dApps chains
-    chains: [mainnet],
-    transports: {
-      // RPC URL for each chain
-      [mainnet.id]: http(
-        `https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_ID}`,
-      ),
-    },
-
     // Required API Keys
     walletConnectProjectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || "",
-
     // Required App Info
-    appName: "Your App Name",
-
-    // Optional App Info
-    appDescription: "Your App Description",
-    appUrl: "https://family.co", // your app's url
-    appIcon: "https://family.co/logo.png", // your app's icon, no bigger than 1024x1024px (max. 1MB)
+    appName: "lemonade.social",
   }),
 );
 
