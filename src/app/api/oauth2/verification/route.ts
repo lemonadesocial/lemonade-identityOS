ba44740f9389c032b570ffa0b1cfa789d2c5c26a
thongxuan <thong.dang@coderpush.com>
2025-08-21 23:40:49 +0700

    feat(verification): add setting & verification routes (#30)

---
+++ src/app/api/oauth2/verification/route.ts
@@ -0,0 +1,50 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { getCsrfToken } from "../../../../client/ory";
+import { frontendApi } from "../../../../common/ory";
+import { addCorsHeaders } from "../../../../server/request";
+
+async function processPost(request: NextRequest) {
+  const body = await request.json();
+
+  const flowId: string | undefined = body.flow_id;
+  const code: string | undefined = body.code;
+  const email: string | undefined = body.email;
+
+  const verificationFlow = !flowId
+    ? await frontendApi.createNativeVerificationFlow({})
+    : await frontendApi.getVerificationFlow({ id: flowId });
+
+  const csrf_token = getCsrfToken(verificationFlow) ?? "";
+
+  let messages: any[] | undefined;
+
+  try {
+    const updatedFlow = await frontendApi.updateVerificationFlow({
+      flow: verificationFlow.id,
+      updateVerificationFlowBody: {
+        csrf_token,
+        method: "code",
+        email,
+        ...(code && { code }),
+      },
+    });
+
+    messages = updatedFlow.ui.messages;
+  } catch (err: any) {
+    const errorFLow = await err.response.json();
+    messages = errorFLow.ui.messages;
+  }
+
+  return NextResponse.json({ flow_id: verificationFlow.id, error: messages }, { status: 200 });
+}
+
+export async function POST(request: NextRequest) {
+  const response = await processPost(request);
+  return addCorsHeaders(response);
+}
+
+export async function OPTIONS() {
+  const response = new NextResponse(null, { status: 200 });
+  return addCorsHeaders(response);
+}
