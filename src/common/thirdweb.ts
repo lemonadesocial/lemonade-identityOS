07df296f5e4e79aca9335307ed5842ef8fc2d606
Thong Dang <thong.dang@coderpush.com>
2026-01-03 08:03:22 +0700

    feat: add thirdweb client id

--- src/common/thirdweb.ts
+++ src/common/thirdweb.ts
@@ -2,7 +2,7 @@ import { mainnet } from "wagmi/chains";
 import { createThirdwebClient, defineChain } from "thirdweb";
 
 const thirdwebClientId =
-  process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID || "4e8c81182c3709ee441e30d776223354";
+  process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID || "";
 
 const client = createThirdwebClient({
   clientId: thirdwebClientId,
